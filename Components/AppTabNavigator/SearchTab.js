b43de1908a1db61ee74e1d8000b2988303c0d88d
0Ams <matoda88@gmail.com>
2020-01-19 20:35:02 +0900

    refact: refactoring indent

--- Components/AppTabNavigator/SearchTab.js
+++ Components/AppTabNavigator/SearchTab.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
-import { 
-    View,
-    Text,
-    StyleSheet
-    } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
 import { Icon } from 'native-base';
 
-class SearchTab extends Component{
-    static navigationOptions = {
-        tabBarIcon: ({ tintColor }) => (
-            < Icon name='ios-search' style={{color:tintColor}} />
-        )
-    }
+class SearchTab extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      < Icon name='ios-search' style={{ color: tintColor }} />
+    )
+  }
 
-    render(){
-        return (
-            <View style={style.container}>
-                <Text>SearchTab</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={style.container}>
+        <Text>SearchTab</Text>
+      </View>
+    );
+  }
 }
 export default SearchTab;
 
 const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 })
--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { 
-    View,
-    Text,
-    Button,
-    StyleSheet,
-    Platform
-    } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Platform
+} from 'react-native';
 
 import { Icon } from 'native-base';
 import { createTabNavigator } from 'react-navigation-tabs';
@@ -16,60 +16,60 @@ import AddMediaTab from './AppTabNavigator/AddMediaTab';
 import LikesTab from './AppTabNavigator/LikesTab';
 import ProfileTab from './AppTabNavigator/ProfileTab';
 
-class MainScreen extends Component{
-    static navigationOptions = {
-        title: 'MainScreen',
-        header: null
-    }
+class MainScreen extends Component {
+  static navigationOptions = {
+    title: 'MainScreen',
+    header: null
+  }
 
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-          <AppTabNavigator />
-        );
-    }
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <AppTabNavigator />
+    );
+  }
 }
 export default MainScreen;
 
 const AppTabNavigator = createTabNavigator({
-    HomeTab:{
-        screen: HomeTab
-    },
-    Search:{
-        screen: SearchTab
-    },
-    AddMedia:{
-        screen: AddMediaTab
-    },
-    Likes:{
-        screen: LikesTab
-    },
-    Profile:{
-        screen: ProfileTab
-    }
+  HomeTab: {
+    screen: HomeTab
+  },
+  Search: {
+    screen: SearchTab
+  },
+  AddMedia: {
+    screen: AddMediaTab
+  },
+  Likes: {
+    screen: LikesTab
+  },
+  Profile: {
+    screen: ProfileTab
+  }
 }, {
-    animationEnabled: true,
-    swipeEnabled: true,
-    tabBarPosition: "bottom",
-    tabBarOptions: {
-        style: {
-            ...Platform.select({
-                android:{
-                    backgroundColor:'white'
-                }
-            })
-        },
-        activeTintColor: '#000',
-        inactiveTintColor: '#d1cece',
-        showLabel: false,
-        showIcon: true,
-    }
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        android: {
+          backgroundColor: 'white'
+        }
+      })
+    },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    showLabel: false,
+    showIcon: true,
+  }
 })
 
 const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 })
